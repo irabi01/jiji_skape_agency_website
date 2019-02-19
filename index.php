c85b392c6fb0362554a4eeff241d9d5af3dae1c1
irabi <irabikassim01@gmail.com>
2019-02-20 00:33:04 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,212 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title></title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/css.css">
+    <link rel="stylesheet" href="assets/w3.css">
+    <link rel="stylesheet" href="assets/font/css/font-awesome.min.css">
+  </head>
+  <body>
+    <section class="home_section" id="home">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <nav class="navbar navbar-expand-lg fixed-top">
+              <a class="navbar-brand" href="#"><img src="assets/images/Jijiskape.jpg" width="70px;" alt="Responsive image"></a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fa fa-list"></i>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarContent">
+                <ul class="navbar-nav ">
+                  <li class="nav-item">
+                    <a class="nav-link" href="#home">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#about">About</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#services">Services</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#contact">Contact</a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+        </div>
+
+        <div class="row text">
+          <div class="col-md-12">
+            <h1>Jiji-Skape Agency Limited</h1>
+            <a href="#contact" class="btn btn-sm contact_us_btn">contact us</a><a href="#about" class="btn btn-sm who_we_are_btn">who we are</a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="user_list">
+      <div class="w3-container">
+        <div class="row no-gutters">
+          <div class="col-md-3">
+           <i class="fa fa-group"></i>
+           <p>100+</p>
+           <h1>Happy Clients</h1>
+          </div>
+          <div class="col-md-3">
+           <i class="fa fa-home"></i>
+           <p>50+</p>
+           <h1>Appartments</h1>
+          </div>
+          <div class="col-md-3">
+           <i class="fa fa-home"></i>
+           <p>100+</p>
+           <h1>House and Villa</h1>
+          </div>
+          <div class="col-md-3">
+           <i class="fa fa-calendar"></i>
+           <p>24/7/365</p>
+           <h1>Woking Hours</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div class="about" id="about">
+      <div class="container">
+        <div class="row about_header">
+          <div class="col-md-8 offset-md-2">
+            <h1>About us</h1>
+            <p>Jiji-skape, born in early 2017 is an agency dealing with renting, selling and developing of properties in tanzania. through public and private partnership initiatives fully supports professional urban planning and development carried by designated specialists.</p>
+            <p>"Your convenience our priority" Jiji offers tailored requirements and makes sure its delivered with finesse, we also offer plots in planned settlements through individual purchases with options of financing through banks and other institutions.</p>
+          </div>
+        </div>
+        <div class="row row_mission_vision">
+          <div class="col-md-6">
+            <div class="vision">
+              <h1>our vision</h1>
+              <p>Ambitious to be one of the most reputable real estate agency in tanzania.</p>
+              <p>We syndicate a team willing to upscale company ambitions and helping jiji reach its potential, we started small but aiming higher which means the sky is not our limit we aim to go beyond it.</p>
+              <p>From improving individual standards of living jijiskape aims to revamp cities and later continents on their ways of living by providing the most innovative residential and commercial developments.</p>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="mission">
+              <h1>our mission</h1>
+              <p>We make sure our clients are fully cartered for with our tailor made services and dedicated employees who ensures maximum satisfcation to our esteemed clients, by reaching out and listening to all your requirements we make sure our objectives are met on time and to your convenience.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="quotation_container">
+      <div class="container">
+        <div class="row">
+          <div class=" col-md-8 offset-md-2">
+            <h1>Jiji-skape Agency</h1>
+            <h2>We build, <span>You live</span> </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="message_from_director">
+      <div class="container">
+        <div class="row no-gutters">
+          <div class="col-md-6">
+
+          </div>
+          <div class="col-md-6">
+            <div class="message_container">
+              <h1>Message from director</h1>
+              <p>Along with all challenges, hard work and triumphs along the way, jijiskape drives to put up the best property development in the country with the priority of maximum client satisfaction.</p>
+              <p>Join us on our journey and experience a spectacular and breathtaking sceneries and nature our country has to offer. dealing with jiji does not only give us the priviledge to offer our avantgarde services but it guarantees you to enjoy our country’s beauty from national parks, white sandy beaches, highest mountains, beautiful weather and our alluring islands.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="services_container" id="services">
+      <div class="container">
+        <h1>Our Services</h1>
+        <div class="row services_row">
+          <div class="col-md-3"></div>
+          <div class="col-md-3">
+            <div class="property_container">
+              <h1>Property Management</h1>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="property_container">
+              <h1>Home For Sell and Rent</h1>
+            </div>
+          </div>
+          <div class="col-md-3"></div>
+        </div>
+        <div class="row services_row">
+          <div class="col-md-3"></div>
+          <div class="col-md-3">
+            <div class="property_container">
+              <h1>Property Insurance</h1>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="property_container">
+              <h1>House Development</h1>
+            </div>
+          </div>
+          <div class="col-md-3"></div>
+        </div>
+      </div>
+    </div>
+
+    <div class="contact_section" id="contact">
+      <div class="container">
+        <h1>Contact us</h1>
+        <div class="row">
+          <div class="col-md-8 offset-md-2">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="email_part">
+                  <i class="fa fa-envelope-o"></i>
+                  <p>info@jijiskape.com</p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="contact_part">
+                  <i class="fa fa-phone"></i>
+                  <p>+255 (0) 666 787878</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer_section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <p>Copyright &copy <?php echo date('Y'); ?>. All rights reserved.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="assets/jqueryfile.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
